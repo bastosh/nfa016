4da24a304e75d6df446403dff308fe7214dc261b
Sébastien Pereda <sebastien.pereda@gmail.com>
2018-01-31 14:00:03 +0100

    Ajout de contenu Portfolio et création formulaire de contact

--- src/js/main.js
+++ src/js/main.js
@@ -29,4 +29,40 @@ function toggleNav() {
 toggleNav();
 }());
 
+$(function(){
+    $(".typed").typed({
+        strings: ["assistant d’édition.","traducteur.","relecteur-correcteur.","chargé de comm’.", "développeur web.","Sébastien Pereda."],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 30,
+        // time before typing starts
+        startDelay: 1200,
+        // backspacing speed
+        backSpeed: 5,
+        // time before backspacing
+        backDelay: 500,
+        // loop
+        loop: false,
+        // false = infinite
+        //loopCount: 5,
+        // show cursor
+        showCursor: true,
+        // character for cursor
+        cursorChar: "|",
+        // attribute to type (null == text)
+        attr: null,
+        // either html or text
+        contentType: 'text',
+        // call when done callback function
+        callback: function() {},
+        // starting callback function before each string
+        preStringTyped: function() {},
+        //callback for every typed string
+        onStringTyped: function() {},
+        // callback for reset
+        resetCallback: function() {}
+    });
+});
+
 $( "#tabs" ).tabs();
